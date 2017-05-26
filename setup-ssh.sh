99eeec86a89c9ceba5ff3e4216641be1bdb7abbc
Yuki Takizawa <y.takizawa64@gmail.com>
2017-05-27 00:30:08 +0900

    Add usage

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,2 +1,11 @@
+if [ $# -ne 1 ]; then
+  # If shell is zsh, $0 is used
+  script_full_path=${BASH_SOURCE:-$0}
+  echo "Usage : source $script_full_path <ssh private key>"
+  return 2> /dev/null
+  exit 1
+fi
+
 eval `ssh-agent`
+
 ssh-add $1
